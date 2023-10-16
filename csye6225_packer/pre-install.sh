8b8fc3bf93b9faf1b853313d0ae2c52a45c6b114
Sijian Wang <sijian250@gmail.com>
2023-10-16 14:56:33 -0400

    packer

---
+++ csye6225_packer/pre-install.sh
@@ -0,0 +1,12 @@
+sudo apt update
+echo "install JDK"
+sudo apt install default-jdk
+
+echo "install mariadb"
+sudo apt install mariadb-server
+
+echo "install maven"
+sudo apt install maven
+
+echo "------------------------------"
+echo "All the dependencies installed"
