7b8f4118fde06a1b56be6eb605af74350be6c38e
sijianneu <114265018+sijianneu@users.noreply.github.com>
2023-10-17 14:12:14 -0400

    Update pre-install.sh

--- csye6225_packer/pre-install.sh
+++ csye6225_packer/pre-install.sh
@@ -1,12 +1,23 @@
 sudo apt update
 echo "install JDK"
-sudo apt install default-jdk
+sudo apt -y install default-jdk
 
 echo "install mariadb"
-sudo apt install mariadb-server
+sudo apt -y install mariadb-server 
+echo "secure_installation"
+sudo mysql_secure_installation <<EOF
+
+y
+Wsj13191867918
+Wsj13191867918
+y
+y
+y
+y
+EOF
 
 echo "install maven"
-sudo apt install maven
+sudo apt -y install maven
 
 echo "------------------------------"
 echo "All the dependencies installed"
