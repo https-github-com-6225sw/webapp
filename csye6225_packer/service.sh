a906ade80f11053c8786801f673d13e72a5caa84
sijianneu <wang.sijian1@northeastern.edu>
2023-10-27 17:35:52 -0400

    test3

--- csye6225_packer/service.sh
+++ csye6225_packer/service.sh
@@ -1,6 +1,3 @@
-sudo groupadd csye6225
-sudo useradd -s /bin/false -g csye6225 -d /opt/csye6225 -m csye6225
-
 sudo systemctl daemon-reload
 sudo systemctl enable app.service
 sudo systemctl start app.service
