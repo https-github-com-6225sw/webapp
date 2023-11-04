c70f13b2a523785272a621219fdc1d28ffde8d32
sijianneu <wang.sijian1@northeastern.edu>
2023-11-04 19:48:27 -0400

    try1

--- csye6225_packer/cloudwatch.sh
+++ csye6225_packer/cloudwatch.sh
@@ -1,3 +1,3 @@
-sudo yum install amazon-cloudwatch-agent -y
+sudo apt -y install amazon-cloudwatch-agent
 
 sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -s -c file:/opt/amazon-cloudwatch-config.json
